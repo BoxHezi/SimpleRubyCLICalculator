9423b83f4bc13fc451ee4f59d11519423ae63d59
Boxuan Lu <547181293@qq.com>
2018-11-20 12:54:00 +1100

    reformat code

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/ruby
 
-#Code execute before the program is run
+# Code execute before the program is run
 BEGIN {
-    puts "Welcome to the Simple Ruby Command Line Calculator"
-    puts ""
+    puts 'Welcome to the Simple Ruby Command Line Calculator'
+    puts ''
 }
 
-#Code execute when the program is terminated
+# Code execute when the program is terminated
 END {
-    puts ""
-    puts "Thanks for using this calculator"
-    puts "Existing..."
+    puts ''
+    puts 'Thanks for using this calculator'
+    puts 'Existing...'
 }
 
-def menu()
-    while (true) do
-        #Ruby heredoc
+def menu
+    loop do
+        # Ruby heredoc
         print <<MENU
 You can do:
 1. Addition
@@ -24,69 +24,66 @@ You can do:
 4. Division
 Q. Quit
 MENU
-        userSelection = readSelection
-        if (userSelection == nil)
-            return
-        end
+        user_selection = read_selection
+        return if user_selection.nil?
     end
 end
 
-def readSelection()
-    selection = 0;
-    while (true) do
-        print "Please enter your selection: "
+def read_selection
+    loop do
+        print 'Please enter your selection: '
         selection = gets.chomp
 
-        #convert user input to upper case
+        # convert user input to upper case
         selection = selection.upcase
-        if (selection == "Q")
-            #return value will be "nil"
+        if selection == 'Q'
+            # return value will be "nil"
             return
         end
 
-        #convert user input to integer
+        # convert user input to integer
         selection = selection.to_i
 
-        #check if user input is in the specific range
-        if (!selection.between?(1, 4))
-            puts "Please enter a number from 1 to 4!"
+        # check if user input is in the specific range
+        if !selection.between?(1, 4)
+            puts 'Please enter a number from 1 to 4!'
             redo
         else
-            first, second = getCalcuateNumber
+            first, second = obtain_cal_num
             result = calculate first, second, selection
-            puts ""
+            puts ''
             return result
         end
     end
 end
 
-def getCalcuateNumber()
-    print "Please input the first nubmer: "
+def obtain_cal_num
+    print 'Please input the first nubmer: '
     first = gets.chomp
     first = first.to_i
-    print "Please input the second number: "
+    print 'Please input the second number: '
     second = gets.chomp
     second = second.to_i
 
-    #multiple return value
+    # multiple return value
     return first, second
 end
 
 def calculate(a, b, indicator)
     result = 0
-    if (indicator == 1)
+    if indicator == 1
         result = a + b
-        puts "The result of addition is: #{result}"
-    elsif (indicator == 2)
+        puts "The sum of #{a} and #{b} is: #{result}"
+    elsif indicator == 2
         result = a - b
-        puts "The result of substraction is: #{result}"
-    elsif (indicator == 3)
+        puts "The substraction of #{a} and #{b} is: #{result}"
+    elsif indicator == 3
         result = a * b
-        puts "The result of multiplication is: #{result}"
-    elsif (indicator == 4)
-        #convert one value to floating point into to perform division
+        puts "The multiplication #{a} and #{b} is: #{result}"
+    elsif indicator == 4
+        # convert one value to floating point into to perform division
         result = a.to_f / b
-        puts "The result of division is: #{result}"
+        puts "The division #{a} and #{b} is: #{result}"
     end
     return result
 end
